78e2e5dc8bc9817b1fdb6fb11b8eb3ca1925335d
Muhammad Irfan <10283@contoso.local>
2023-06-22 00:28:49 +0500

    created project

---
+++ assets/js/function.js
@@ -0,0 +1,8 @@
+// Banner Text Change start 
+var typing = new Typed(".annimationtext", {
+    strings: ["", "M.Irfan", "Front End Developer", "Freelancer", "Web Designer",],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true,
+});
+// Banner Text Change end
